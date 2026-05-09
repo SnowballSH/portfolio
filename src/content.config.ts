2753dfc78a7168272c49b15ac0a75978e4d1470c
SnowballSH <derek070211@gmail.com>
2026-05-09 19:41:20 +0800

    try to migrate to astro 6.0

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const projects = defineCollection({
-  type: "content",
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     shortDescription: z.string(),
@@ -35,7 +37,7 @@ const projects = defineCollection({
 });
 
 const blogs = defineCollection({
-  type: "content",
+  loader: glob({ base: "./src/content/blogs", pattern: "**/*.typ" }),
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
